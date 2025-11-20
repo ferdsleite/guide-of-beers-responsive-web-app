b6ebd0c0a1da69ff6acd4575924cc526154b0845
Fernanda <ferdsleite@gmail.com>
2025-11-20 16:23:56 +0100

    applied a correction on sidebar to remove the accessibility warnings

--- src/components/template/Sidebar.svelte
+++ src/components/template/Sidebar.svelte
@@ -1,5 +1,16 @@
 <script>
-  import { IconBeer, IconBeerFilled, IconBrandFacebook, IconBrandInstagram, IconBrandYoutube, IconBuildingCastle, IconHome, IconMenu2, IconNews, IconX } from "@tabler/icons-svelte";
+  import {
+    IconBeer,
+    IconBeerFilled,
+    IconBrandFacebook,
+    IconBrandInstagram,
+    IconBrandYoutube,
+    IconBuildingCastle,
+    IconHome,
+    IconMenu2,
+    IconNews,
+    IconX,
+  } from "@tabler/icons-svelte";
   import Menu from "./Menu.svelte";
   import MenuItem from "./MenuItem.svelte";
 
@@ -11,79 +22,77 @@
   function close() {
     isOpen = false;
   }
-
 </script>
 
 <!-- Desktop Sidebar -->
-<aside class="hidden md:flex flex-col self-start gap-10 w-72 p-5 border border-zinc-800 rounded-md">
-    <Menu title="Browse">
-        <MenuItem href="/" text="Home">
-            <IconHome />
-        </MenuItem>
+<aside
+  class="hidden md:flex flex-col self-start gap-10 w-72 p-5 border border-zinc-800 rounded-md"
+>
+  <Menu title="Browse">
+    <MenuItem href="/" text="Home">
+      <IconHome />
+    </MenuItem>
 
-        <MenuItem href="/breweries" text="Breweries">
-            <IconBuildingCastle />
-        </MenuItem> 
+    <MenuItem href="/breweries" text="Breweries">
+      <IconBuildingCastle />
+    </MenuItem>
 
-        <MenuItem href="/beer-styles" text="Beer Styles"> 
-            <IconBeer />
-        </MenuItem>
+    <MenuItem href="/beer-styles" text="Beer Styles">
+      <IconBeer />
+    </MenuItem>
 
-        <MenuItem href="/" text="Beers">
-            <IconBeerFilled />
-        </MenuItem>
-
-    </Menu>
-    <Menu title="Popular Beer Styles">
-        <MenuItem href="/beer-styles/ipa" text="IPA"/>
-        <MenuItem href="/beer-styles/pilsner" text="Pilsner"/>
-        <MenuItem href="/beer-styles/stout" text="Stout"/>
-        <MenuItem href="/beer-styles/weissbier" text="Weissbier"/>
-        
-    </Menu>
-    <Menu title="Highlighted Breweries">
-        <MenuItem href="/breweries/guinness" text="Guinness"/>
-        <MenuItem href="/breweries/brewdog" text="Brewdog"/>
-        <MenuItem href="/breweries/birra-laru" text="Birra Laru"/>
-        <MenuItem href="/breweries/lzo" text="LZO"/>
-    </Menu>
-    <Menu title="Follow us">
-        <MenuItem href="/" text="Last News">
-            <IconNews />
-        </MenuItem>
-        <MenuItem href="/" text="Instagram">
-            <IconBrandInstagram />
-        </MenuItem>
-        <MenuItem href="/" text="Facebook">
-            <IconBrandFacebook />
-        </MenuItem>
-        <MenuItem href="/" text="Youtube">
-            <IconBrandYoutube />
-        </MenuItem>
-    </Menu>
+    <MenuItem href="/" text="Beers">
+      <IconBeerFilled />
+    </MenuItem>
+  </Menu>
+  <Menu title="Popular Beer Styles">
+    <MenuItem href="/beer-styles/ipa" text="IPA" />
+    <MenuItem href="/beer-styles/pilsner" text="Pilsner" />
+    <MenuItem href="/beer-styles/stout" text="Stout" />
+    <MenuItem href="/beer-styles/weissbier" text="Weissbier" />
+  </Menu>
+  <Menu title="Highlighted Breweries">
+    <MenuItem href="/breweries/guinness" text="Guinness" />
+    <MenuItem href="/breweries/brewdog" text="Brewdog" />
+    <MenuItem href="/breweries/birra-laru" text="Birra Laru" />
+    <MenuItem href="/breweries/lzo" text="LZO" />
+  </Menu>
+  <Menu title="Follow us">
+    <MenuItem href="/" text="Last News">
+      <IconNews />
+    </MenuItem>
+    <MenuItem href="/" text="Instagram">
+      <IconBrandInstagram />
+    </MenuItem>
+    <MenuItem href="/" text="Facebook">
+      <IconBrandFacebook />
+    </MenuItem>
+    <MenuItem href="/" text="Youtube">
+      <IconBrandYoutube />
+    </MenuItem>
+  </Menu>
 </aside>
 
 <!-- Tablet and Mobile Menu Icon -->
- <div class="flex md:hidden justify-end items-center mb-6">
-    <button 
-        type="button"
-        class="text-zinc-200 bg-zinc-900 p-1.5 rounded-md"  
-        aria-expanded={isOpen} 
-        aria-controls="mobile-sidebar" 
-        on:click={toggle}
-    >
-        {#if !isOpen}
-            <IconMenu2 size={26}/>
-        {:else}
-            <IconX size={26} />
-        {/if}
-    </button>
- </div>
+<div class="flex md:hidden justify-end items-center mb-6">
+  <button
+    type="button"
+    class="text-zinc-200 bg-zinc-900 p-1.5 rounded-md"
+    aria-expanded={isOpen}
+    aria-controls="mobile-sidebar"
+    on:click={toggle}
+  >
+    {#if !isOpen}
+      <IconMenu2 size={26} />
+    {:else}
+      <IconX size={26} />
+    {/if}
+  </button>
+</div>
 
- <!-- Mobile Menu Drawer -->
+<!-- Mobile Menu Drawer -->
 {#if isOpen}
   <div class="fixed inset-0 z-50 flex justify-end" role="presentation">
-
     <!-- Bg overlay closes on click -->
     <button
       type="button"
@@ -94,60 +103,68 @@
 
     <!-- Sidebar drawer -->
     <div
-        id="mobile-sidebar"
-        class="relative w-64 h-full bg-zinc-900 border-l border-zinc-800 p-5 flex flex-col gap-8"
-        on:click|stopPropagation
-        aria-label="Mobile Sidebar"
+      id="mobile-sidebar"
+      class="relative w-64 h-full bg-zinc-900 border-l border-zinc-800 p-5 flex flex-col gap-8"
+      role="dialog"
+      aria-modal="true"
+      aria-label="Mobile navigation menu"
+      tabindex="-1"
+      on:click|stopPropagation
+      on:keydown|stopPropagation
     >
       <div class="flex justify-end">
-        <button type="button" class="text-zinc-200" aria-label="Close menu" on:click={close}>
+        <button
+          type="button"
+          class="text-zinc-200"
+          aria-label="Close menu"
+          on:click={close}
+        >
           <IconX />
         </button>
-      </div>    
-        <Menu title="Browse">
-            <MenuItem href="/" text="Home">
-                <IconHome />
-            </MenuItem>
-            
-            <MenuItem href="/breweries" text="Breweries">
-                <IconBuildingCastle />
-            </MenuItem> 
-            
-            <MenuItem href="/beer-styles" text="Beer Styles"> 
-                <IconBeer />
-            </MenuItem>
-            
-            <MenuItem href="/" text="Beers">
-                <IconBeerFilled />
-            </MenuItem>           
-        </Menu>
-            <Menu title="Popular Beer Styles">
-                <MenuItem href="/beer-styles/ipa" text="IPA"/>
-                <MenuItem href="/beer-styles/pilsner" text="Pilsner"/>
-                <MenuItem href="/beer-styles/stout" text="Stout"/>
-                <MenuItem href="/beer-styles/weissbier" text="Weissbier"/>       
-            </Menu>
-                <Menu title="Highlighted Breweries">
-                    <MenuItem href="/breweries/guinness" text="Guinness"/>
-                    <MenuItem href="/breweries/brewdog" text="Brewdog"/>
-                    <MenuItem href="/breweries/birra-laru" text="Birra Laru"/>
-                    <MenuItem href="/breweries/lzo" text="LZO"/>
-                </Menu>
-                <Menu title="Follow us">
-                    <MenuItem href="/" text="Last News">
-                        <IconNews />
-                    </MenuItem>
-                    <MenuItem href="/" text="Instagram">
-                        <IconBrandInstagram />
-                    </MenuItem>
-                    <MenuItem href="/" text="Facebook">
-                        <IconBrandFacebook />
-                    </MenuItem>
-                    <MenuItem href="/" text="Youtube">
-                        <IconBrandYoutube />
-                    </MenuItem>
-                </Menu>
+      </div>
+      <Menu title="Browse">
+        <MenuItem href="/" text="Home">
+          <IconHome />
+        </MenuItem>
 
-            </div>
+        <MenuItem href="/breweries" text="Breweries">
+          <IconBuildingCastle />
+        </MenuItem>
+
+        <MenuItem href="/beer-styles" text="Beer Styles">
+          <IconBeer />
+        </MenuItem>
+
+        <MenuItem href="/" text="Beers">
+          <IconBeerFilled />
+        </MenuItem>
+      </Menu>
+      <Menu title="Popular Beer Styles">
+        <MenuItem href="/beer-styles/ipa" text="IPA" />
+        <MenuItem href="/beer-styles/pilsner" text="Pilsner" />
+        <MenuItem href="/beer-styles/stout" text="Stout" />
+        <MenuItem href="/beer-styles/weissbier" text="Weissbier" />
+      </Menu>
+      <Menu title="Highlighted Breweries">
+        <MenuItem href="/breweries/guinness" text="Guinness" />
+        <MenuItem href="/breweries/brewdog" text="Brewdog" />
+        <MenuItem href="/breweries/birra-laru" text="Birra Laru" />
+        <MenuItem href="/breweries/lzo" text="LZO" />
+      </Menu>
+      <Menu title="Follow us">
+        <MenuItem href="/" text="Last News">
+          <IconNews />
+        </MenuItem>
+        <MenuItem href="/" text="Instagram">
+          <IconBrandInstagram />
+        </MenuItem>
+        <MenuItem href="/" text="Facebook">
+          <IconBrandFacebook />
+        </MenuItem>
+        <MenuItem href="/" text="Youtube">
+          <IconBrandYoutube />
+        </MenuItem>
+      </Menu>
     </div>
-  {/if}
+  </div>
+{/if}
